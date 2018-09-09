e6ed12c22deb4727bb7fd3f76d75196b0d3880ae
harrisgeorge202 <harrisgeorge202@gmail.com>
2018-09-10 00:00:45 +0530

    first commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,32 @@
-console.log("It works!");
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+
+
+import { Header } from './components/Header'
+import { Home } from './components/Home'
+import { Root } from './components/Root'
+import { User } from './components/User';
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path={'/'} component={Root}>
+                <IndexRoute component={Home}/>
+                <Route path={'user/:id'} component={User}/>
+                <Route path={'home'} component={Home}/>
+                
+                </Route>
+                <Route path={'home2'} component={Home}/>
+                
+            </Router>
+        )
+    }
+
+}
+
+
+render(<App />, window.document.getElementById('app'));
